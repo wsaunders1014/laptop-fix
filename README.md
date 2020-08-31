68a7852744df9a11d3f18e0df6b2cd08530b0672
Wsaunders1014 <willsaunders1014@gmail.com>
2020-08-31 17:47:57 -0400

    0.0.2 update: Fix mythicUI module conflict

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # laptop-fix
+
+# 0.0.2
+Fixed conflict with MythicUI module as reported by user.
+
+# Alpha
 Fixes issues where the SceneConfig and FilePicker Form Applications put the submit button off screen on laptops. Rearranges html/css so that the form apps are never taller than window.
--- laptop-fix.js
+++ laptop-fix.js
@@ -1,10 +1,23 @@
 Hooks.on('init',() => {;
 	console.log('Laptop Fix')
-	//CONFIG.debug.hooks = true;
+	CONFIG.debug.hooks = true;
 })
 Hooks.on('renderSceneConfig',(object,html)=>{
 	//console.log(object,html);
 	html.find('form').wrapInner('<div class="scroll"></div>')
 	let button = html.find('button[name="submit"]');
 	html.find('form').append(button);
+	if(typeof game.modules.get('MythicUI') !='undefined'){
+		if(game.modules.get('MythicUI').active){
+			html.find('form').addClass('mythicUI-fix')
+		}
+	}
+})
+Hooks.on('renderFilePicker',(object,html)=>{
+	console.log(html)
+	if(typeof game.modules.get('MythicUI') !='undefined'){
+		if(game.modules.get('MythicUI').active){
+			html.addClass('mythicUI-fix')
+		}
+	}
 })
